7c8c2c777b723a514285a65107ac8f74d2864611
nDoorn <ndoorn@outlook.com>
2021-07-30 16:33:54 +0200

    remove commented logging

--- statemachine/PacketEvent.go
+++ statemachine/PacketEvent.go
@@ -48,8 +48,6 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
 
   if csm.AvailableData() >= sizeHeaderAndEventType {
     wantedBytes := csm.UnprocessedBuffer[sizeHeader:sizeHeaderAndEventType]
-    // fmt.Printf("wanted bytes: %+v | %x %s\n", wantedBytes, wantedBytes, wantedBytes)
-    // fmt.Printf("event type is %s\n", wantedBytes)
 
     text := string(wantedBytes)
 
@@ -58,8 +56,6 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
       fastestLap := FastestLap{}
       if csm.AvailableData() >= GetMemorySize(fastestLap)+sizeHeaderAndEventType {
         ToObject(csm.UnprocessedBuffer[sizeHeaderAndEventType:], &fastestLap)
-        // fmt.Printf("event: fastestLap triggered for id:%d, time:%.2f\n", fastestLap.VehicleIndex, fastestLap.LapTime)
-        // state.FastestLap = fastestLap
         csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType + GetMemorySize(fastestLap), state)
       }
       break
@@ -68,7 +64,6 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
       retirement := Retirement{}
       if csm.AvailableData() >= GetMemorySize(retirement)+sizeHeaderAndEventType {
         ToObject(csm.UnprocessedBuffer[sizeHeaderAndEventType:], &retirement)
-        // fmt.Printf("event: retirement triggered for id:%d\n", retirement.VehicleIndex)
         csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType + GetMemorySize(retirement), state)
       }
       break
@@ -77,7 +72,6 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
       teamMateInPits := TeamMateInPits{}
       if csm.AvailableData() >= GetMemorySize(teamMateInPits)+sizeHeaderAndEventType {
         ToObject(csm.UnprocessedBuffer[sizeHeaderAndEventType:], &teamMateInPits)
-        // fmt.Printf("event: teamMateInPits triggered for id:%d\n", teamMateInPits.VehicleIndex)
         csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType + GetMemorySize(teamMateInPits), state)
       }
       break
@@ -86,7 +80,6 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
       raceWinner := RaceWinner{}
       if csm.AvailableData() >= GetMemorySize(raceWinner)+sizeHeaderAndEventType {
         ToObject(csm.UnprocessedBuffer[sizeHeaderAndEventType:], &raceWinner)
-        // fmt.Printf("event: raceWinner triggered for id:%d\n", raceWinner.VehicleIndex)
         csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType + GetMemorySize(raceWinner), state)
       }
       break
@@ -95,7 +88,6 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
       penalty := Penalty{}
       if csm.AvailableData() >= GetMemorySize(penalty)+sizeHeaderAndEventType {
         ToObject(csm.UnprocessedBuffer[sizeHeaderAndEventType:], &penalty)
-        // fmt.Printf("event: penalty triggered for id:%d, object:%+v\n", penalty.VehicleIndex, penalty)
         csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType + GetMemorySize(penalty), state)
       }
       break
@@ -105,41 +97,28 @@ func ProcessPacketEvent(csm *CommunicationStateMachine, state *StateMachine) {
       sizeTrap := GetMemorySize(trap)
       if csm.AvailableData() >= sizeTrap+sizeHeaderAndEventType {
         ToObject(csm.UnprocessedBuffer[sizeHeaderAndEventType:], &trap)
-        // fmt.Printf("event: speed trap triggered for id:%d, speed %.2f\n", trap.VehicleIndex, trap.Speed)
         state.SpeedTraps[trap.VehicleIndex] = trap
         csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType + sizeTrap, state)
-      } else {
-        // fmt.Printf(
-        //   "not enough data for speed trap. trap-mem:%d, size header and event type:%d, available %d\n",
-        //   sizeTrap,
-        //   sizeHeaderAndEventType,
-        //   state.AvailableData(),
-        // )
       }
       break
 
     case "CHQF":
-      // fmt.Println("event: chequered flag, no additional data.")
       csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType, state)
       break
 
     case "DRSE":
-      // fmt.Println("event: DRS enabled, no additional data.")
       csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType, state)
       break
 
     case "DRSD":
-      // fmt.Println("event: DRS disabled, no additional data.")
       csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType, state)
       break
 
     case "SEND":
-      // fmt.Println("event: end of session, no additional data.")
       csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType, state)
       break
 
     case "SSTA":
-      // fmt.Println("event: start session, no additional data.")
       csm.RemoveFirstBytesFromBuffer(sizeHeaderAndEventType, state)
       state.ResetTimers()
       break
